ee77c6c6cf4f5d874e952bad98445de525a6a1cc
USER <USER@USER-PC>
2017-03-17 16:26:03 +0800

    加入控制器

---
+++ CRM/Models/sysdiagramsRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+	
+namespace CRM.Models
+{   
+	public  class sysdiagramsRepository : EFRepository<sysdiagrams>, IsysdiagramsRepository
+	{
+
+	}
+
+	public  interface IsysdiagramsRepository : IRepository<sysdiagrams>
+	{
+
+	}
+}
